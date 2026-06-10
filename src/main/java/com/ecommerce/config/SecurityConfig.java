1a356ca3de0eeb8964eb4a93251cb09bfcb5a7fd
gamechangerSumit <sumitspandey1609@gmail.com>
2026-06-10 22:29:43 +0530

    fix cqrs file3

--- src/main/java/com/ecommerce/config/SecurityConfig.java
+++ src/main/java/com/ecommerce/config/SecurityConfig.java
@@ -24,16 +24,16 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS yahi se chalega
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() // Register/Login open
                         .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll() // GET sab kar sakte
-                        .requestMatchers(HttpMethod.POST, "/api/products/**").permitAll()// POST ke liye bas login hona chahiye
+                        .requestMatchers(HttpMethod.POST, "/api/products/**").permitAll() // POST ke liye bas login hona chahiye
                         .requestMatchers("/api/cart/**").permitAll()
                         .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()
                         .anyRequest().authenticated()
                 );
-                //.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // Ye line comment kar de agar JWT filter nahi hai
+        //.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // JWT filter comment me rehne de
 
         return http.build();
     }
@@ -41,10 +41,19 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173", "https://zqh7vfkl-5173.inc1.devtunnels.ms" ));
+
+        // Yaha Netlify + Localhost dono add kar diye
+        configuration.setAllowedOriginPatterns(List.of(
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "https://shopkaro-shopping.netlify.app",
+                "https://*.netlify.app"  // Netlify ke preview/deploy branches ke liye
+        ));
+
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
